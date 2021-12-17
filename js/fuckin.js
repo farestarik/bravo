ff0745144626ddfe18551903c1dfc3addabbc4b4
powertiger7 <powertiger563@gmail.com>
2021-12-17 03:00:02 +0200

    Spaces

--- js/fuckin.js
+++ js/fuckin.js
@@ -8,36 +8,45 @@ let video_markup = `
 $("#show_media_btn").on("click", function(e) {
     e.preventDefault();
 
-    let media = {
-        "برافو علييييك": "videos/bravo.mp4",
-        "هو ده": "videos/hayda_howa.mp4"
-    };
+    if (!$.trim($("#msg").val())) {
+        alert('عيب كده يسطا');
+        $("#msg").val('');
+    } else {
 
-    let media_length = media.length;
+        let media = {
+            "برافو علييييك": "videos/bravo.mp4",
+            "هو ده": "videos/hayda_howa.mp4"
+        };
 
-    // var vid = media[Math.floor(Math.random() * media_length)];
+        let media_length = media.length;
 
-    let header = function(object, value) {
-        return Object.keys(object).find(key => object[key] === value);
-    };
+        // var vid = media[Math.floor(Math.random() * media_length)];
 
-    let random = function(obj, out = 'vid') {
-        var keys = Object.keys(obj);
-        return obj[keys[keys.length * Math.random() << 0]];
-    };
+        let header = function(object, value) {
+            return Object.keys(object).find(key => object[key] === value);
+        };
 
-    let vid = random(media);
-    let media_label = header(media, vid);
+        let random = function(obj, out = 'vid') {
+            var keys = Object.keys(obj);
+            return obj[keys[keys.length * Math.random() << 0]];
+        };
 
-    $("#media_modal").modal('show');
+        let vid = random(media);
+        let media_label = header(media, vid);
 
-    $("#fuckin_media_box").html(video_markup);
-    $("#video_src").attr('src', vid);
-    $("#media_label").html(media_label);
+        $("#media_modal").modal('show');
 
-    $('video').on('ended', function(e) {
-        $("#teslam_btn").click();
-    });
+        $("#fuckin_media_box").html(video_markup);
+        $("#video_src").attr('src', vid);
+        $("#media_label").html(media_label);
+
+        $('video').on('ended', function(e) {
+            $("#teslam_btn").click();
+        });
+
+
+    }
+
 
 });
 
